9d07940ff4b19310d1fcf7edc7e7abd079f81f88
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-04-25 16:33:26 -0400

    wokring on plotting from json

--- static/js/piechart.js
+++ static/js/piechart.js
@@ -1,7 +1,7 @@
 var data = [{
   type: "pie",
-  values: [2, 3, 4, 4],
-  labels: ["MODEL1", "MODEL2", "MODEL3", "MODEL4"],
+  values: [2, 3, 4, 4, 6],
+  labels: ["MODEL1", "MODEL2", "MODEL3", "MODEL4", "MODEL5"],
   textinfo: "label+percent",
   textposition: "inside",
   automargin: true
@@ -11,7 +11,9 @@ var layout = {
   height: 400,
   width: 400,
   margin: {"t": 0, "b": 0, "l": 0, "r": 0},
-  showlegend: false
+  showlegend: false,
+  animate: true
+    
   }
 
 Plotly.newPlot('pieplot', data, layout)
