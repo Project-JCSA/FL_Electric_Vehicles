908a611f9d8808b04e87a267787cb98bb822ee4d
Christina Gangi <christina@buzzella.com>
2021-04-25 18:29:13 -0400

    Fixed typos found during group meeting.

--- app.py
+++ app.py
@@ -10,15 +10,32 @@ app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 db_path = os.path.abspath('./static/data/sqlite.db')
+
+
+# Code for testing database connection
 conn = sqlite3.connect(db_path)
-# cursor = conn.cursor()
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM demographic')
+myData = cursor.fetchall()
+conn.close()
+print(myData)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    myData = pd.read_sql('''SELECT * FROM demographic''', conn).to_json()
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    # myData = pd.read_sql('''SELECT * FROM demographic''', conn).to_json()
+    # myData = pd.read_sql('''SELECT * FROM demographic''', conn)
+    cursor.execute('SELECT * FROM demographic')
+    myData = cursor.fetchall()
+
     print(myData)
-    return render_template("text.index", myData=myData)
+
+    if conn:
+        conn.close()
+
+    return render_template("index.html", myData=myData)
 
 if __name__ == "__main__":
     app.run(debug=True)
